055ad540d5530d4d22228ed886025bb2ae25a082
Chuck2Win <53412437+Chuck2Win@users.noreply.github.com>
2021-04-01 14:57:31 +0900

    Add files via upload

--- train_model.py
+++ train_model.py
@@ -14,15 +14,13 @@ from lightgbm import LGBMRegressor
 import pickle
 
 parser = argparse.ArgumentParser(description = 'model train')
-parser.add_argument('--models', type = list , default = ['rf','xg','lgbm'] , help = '모델')
+
 parser.add_argument('--test_size', type = float , default = 0.3 , help = 'test_size')
-parser.add_argument('--test_type', type = str , default = 'else' , help = 'sequential이면 마지막 test size만큼을 남김')
-#parser.add_argument('--k', type = int , default = 10 , help = 'cross validation')
-parser.add_argument('--data', type = str , default = 'G:/넥센타이어/Foot Print/preprocessed_data.csv' , help = '데이터 저장 위치')
+parser.add_argument('--test_type', type = str , default = 'sequential' , help = 'sequential이면 마지막 test size만큼을 남김')
+parser.add_argument('--data', type = str , default = './data/preprocessed_data.csv' , help = '데이터 저장 위치')
 parser.add_argument('--report', type = str , default = 'report' , help = '데이터 저장 위치')
-parser.add_argument('--memo', type = str , default = 'G:/넥센타이어/Foot Print/onehot_columns_memo' , help = 'Onehotvector 이름')
-parser.add_argument('--ys',type = list, default = ['Data60','Data61','Data62','Data63'], help ='종속변수들')
-parser.add_argument('--except_x',type = list, default = ['Data57'], help ='불필요한 변수들')
+parser.add_argument('--target_feature_names',type = list, default = ['Data60','Data61','Data62','Data63'], help ='종속변수들')
+parser.add_argument('--except_feature_names',type = list, default = ['Data57'], help ='제외시킬 변수들')
 
 
 def MAPE(y_true,y_pred):
@@ -35,77 +33,75 @@ def MAPE(y_true,y_pred):
     '''
     return np.mean(abs((y_true-y_pred)/(y_true+1e-8)))*100
 
-class train_models:
+class train_model:
     def __init__(self):
         self.scores = {}
-        self.save_models ={}
-        self.models = args.models
-        for target in args.ys:
+        for target in args.target_feature_names:
             self.scores[target]={}
-            self.save_models[target]={}
-        
+        self.save_model = {target:None for target in args.target_feature_names}
     def train(self):
-        for target in args.ys:
-            for name in self.models:
-             # 현행 방향 - 종속변수끼리의 교호 작용 파악 x
-                self.scores[target][name]={'train':{},'test':{}}
-                train_y = train.loc[:,target] 
-                train_x = train.loc[:,train.columns.difference(args.ys+args.except_x)]
-                test_y = test.loc[:,target] 
-                test_x = test.loc[:,test.columns.difference(args.ys+args.except_x)]
-                if name == 'xg':
-                    model = XGBRegressor()
-                elif name =='rf':
-                    model = RandomForestRegressor(max_features=int(math.sqrt(train_x.shape[1]/3)), min_samples_leaf=1,max_depth=30,n_estimators=100, oob_score=True, n_jobs=-1)
-                elif name == 'lgbm':
-                    model = LGBMRegressor()
-                model.fit(train_x,train_y)
-                train_pred = model.predict(train_x)
-                self.scores[target][name]['train']['mse']=mean_squared_error(train_y,train_pred)
-                self.scores[target][name]['train']['mape']=MAPE(train_y,train_pred)
-                self.scores[target][name]['train']['r2']=r2_score(train_y,train_pred)
-                test_pred = model.predict(test_x)
-                self.scores[target][name]['test']['mse']=mean_squared_error(test_y,test_pred)
-                self.scores[target][name]['test']['mape']=MAPE(test_y,test_pred)
-                self.scores[target][name]['test']['r2']=r2_score(test_y,test_pred)
-                self.save_models[target][name]=model
+        for target in args.target_feature_names:
+            model = LGBMRegressor() # 여기만 변경해주면 됨.
+            
+            self.scores[target]={'train':{},'test':{}}
+            train_y = train.loc[:,target] 
+            train_x = train.loc[:,train.columns.difference(args.target_feature_names+args.except_feature_names)]
+            test_y = test.loc[:,target] 
+            test_x = test.loc[:,test.columns.difference(args.target_feature_names+args.except_feature_names)]
+            model.fit(train_x,train_y)
+            train_pred = model.predict(train_x)
+            self.scores[target]['train']['rmse']=np.sqrt(mean_squared_error(train_y,train_pred))
+            self.scores[target]['train']['mape']=MAPE(train_y,train_pred)
+            self.scores[target]['train']['r2']=r2_score(train_y,train_pred)
+            test_pred = model.predict(test_x)
+            self.scores[target]['test']['rmse']=np.sqrt(mean_squared_error(test_y,test_pred))
+            self.scores[target]['test']['mape']=MAPE(test_y,test_pred)
+            self.scores[target]['test']['r2']=r2_score(test_y,test_pred)
+            self.save_model[target]=model
+            
+            
     def save_scores(self):
         try:
             os.mkdir(args.test_type)
         except:
             pass
         os.chdir(args.test_type)
-        for target in args.ys:
+        
+        
+        for target in args.target_feature_names:
             d = {}
-            for i in self.scores[target].keys(): # model
+            for i in self.scores[target].keys(): # train,test
                 y = []
                 d[i]=[]
-                for j in self.scores[target][i].keys(): # train, test
-                    
-                    for r,value in self.scores[target][i][j].items(): # mse,r2,mape
-                        d[i].append(value)
-                        y.append(j+'_'+r)
+                for r,value in self.scores[target][i].items(): # mse,r2,mape
+                    d[i].append(value)
+                    y.append(r)
             report = df(d,index = y) 
             report.to_csv('./report+%s.csv'%target, index = True)
+        
+        
         os.chdir('..')
   
 # train, test split
-# Kfold Validation
+
 if __name__=='__main__':
     args=parser.parse_args()
-    data = pd.read_csv(args.data,header=0)
+    data = pd.read_csv(args.data,header=0,index_col=0)
+    
+
     data['Data57']=data['Data57'].astype('datetime64')
     if args.test_type == 'sequential':
-        train = data.loc[data['Data57']<=data['Data57'][int(len(data)*args.test_size)],:]
-        test = data.loc[data['Data57']>data['Data57'][int(len(data)*args.test_size)],:]
+        shuffle = False
     else: 
-        train,test=train_test_split(data,test_size = args.test_size, random_state = 123, shuffle = True)
-    t = train_models()
+        shuffle = True
+    train,test=train_test_split(data,test_size=args.test_size,shuffle=shuffle)
+    
+    t = train_model()
     t.train()
     t.save_scores()
-    t.scores
-    os.chdir(args.test_type)
-    f = open('train_model_%s'%args.test_type,'wb')
-    pickle.dump(t,f)
-    f.close()
-    os.chdir('..')
+    F = open('./save_model','wb')
+    pickle.dump(t,F)
+    F.close()
+    
+    
+    
